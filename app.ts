efc904eec4f2cb2a2f85e7ffb9f6f42480accc1e
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-05-13 20:22:59 +0100

    Set up MySQL database connection using dotenv for environment variables and configured Express server

--- app.ts
+++ app.ts
@@ -1,8 +1,30 @@
 import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
 const app = express();
+
+/* ---> Load environment variables <--- */
+dotenv.config();
+
+/* ---> Middlewares <--- */
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Hello World!")
-})
+/* ---> MySQL Database Connection Configuration <--- */
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+/* ---> Connect to the Database and Start Server <--- */
+export default connection.connect((err) => {
+    if (err) {
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    app.listen(3000, () => {
+        console.log("Hello World!");
+    });
+});
